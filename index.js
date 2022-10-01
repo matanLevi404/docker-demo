b4ca79058c2a7f78962eb31f08bc04c460d77607
NoobMajesty <47846708+NoobMajesty@users.noreply.github.com>
2022-10-01 19:33:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('huh!');
+  res.send('how tf this works?!');
 });
 
 var server = app.listen(3000, function () {
